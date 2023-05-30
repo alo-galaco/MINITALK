c86c8ffe80f9c51790f7c39385faf8941b78bbc0
Flavia Cristina De Sales Teles Da Fonseca <flcristi@c1r5p1.42sp.org.br>
2023-05-29 22:10:03 -0300

    init client

--- client.c
+++ client.c
@@ -6,22 +6,68 @@
 /*   By: flcristi <flcristi@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 15:52:49 by flcristi          #+#    #+#             */
-/*   Updated: 2023/05/29 21:21:31 by flcristi         ###   ########.fr       */
+/*   Updated: 2023/05/29 22:06:19 by flcristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main(int argc, char **argv)
-{
-    struct sigaction	action;
-	sigset_t			sigset;
+int	g_bit;
 
-	sigemptyset(&sigset);
-	sigaddset(&sigset, SIGUSR1);
-	sigaddset(&sigset, SIGUSR2);
-	action.sa_handler = NULL;
-	
-	sigaction(SIGUSR1, &action, NULL);
-	sigaction(SIGUSR2, &action, NULL);
+static	void	signal_handler(int sig)
+{
+	if (sig == SIGUSR1)
+		g_bit++;
+	else if (sig == SIGUSR2)
+		g_bit++;
+	else
+	{
+		ft_putstr_fd("\tError", 1)
+		exit(1);
+	}
+}
+
+static	void	client_error(void)
+{
+	ft_putstr_fd("Invalid arguments\n", 1);
+	ft_putstr_fd("./client <server PID> <message>\n", 1);
+	exit(1);
+}
+
+void	bit_by_bit(int server_pid, char c)
+{
+	while (g_bit < 8)
+	{
+		if ((128 >> g_bit) & c)
+			kill(server_pid, SIGUSR2)
+		else
+			kill(server_pid, SIGUSR1);
+		usleep(100);
+		if (g_bit == 8)
+		{
+			g_bit = 0;
+			return ;
+		}
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	size_t	i;
+	int		server_pid;
+
+	if (argc != 3)
+		client_error();
+	g_bit = 0;
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	i = 0;
+	server_pid = ft_atoi(argv[1]);
+	while (argv[2][i] != '\0')
+	{
+		bit_by_bit(server_pid, argv[2][i]);
+		i++;
+	}
+	else
+		client_error();
 }
--- server.c
+++ server.c
@@ -6,25 +6,25 @@
 /*   By: flcristi <flcristi@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 15:52:52 by flcristi          #+#    #+#             */
-/*   Updated: 2023/05/29 21:05:18 by flcristi         ###   ########.fr       */
+/*   Updated: 2023/05/29 21:40:27 by flcristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void server_error(void)
+static	void	server_error(void)
 {
 	ft_putstr_fd("Invalid arguments\n", 1);
 	ft_putstr_fd("HELP", 1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    struct sigaction	action;
+	struct sigaction	action;
 	sigset_t			sigset;
 
 	(void)argv;
-	if (argc != 3)
+	if (argc != 1)
 	{
 		server_error();
 		exit(1);
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
 	sigaddset(&sigset, SIGUSR1);
 	sigaddset(&sigset, SIGUSR2);
 	action.sa_handler = NULL;
-	
+	sig.sa_mask = sigset;
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
 }
